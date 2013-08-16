873646f7d48ae9738574733adc0073a815c991ee
jun <hyun2012@gmail.com>
2013-08-16 13:59:53 +0900

    편집 뷰를 위한 EditViewController 클래스 생성

---
+++ BirthdayReminder/EditViewController.h
@@ -0,0 +1,13 @@
+//
+//  EditViewController.h
+//  BirthdayReminder
+//
+//  Created by lovejun on 2013. 8. 16..
+//  Copyright (c) 2013년 jun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EditViewController : UIViewController
+
+@end
---
+++ BirthdayReminder/EditViewController.m
@@ -0,0 +1,38 @@
+//
+//  EditViewController.m
+//  BirthdayReminder
+//
+//  Created by lovejun on 2013. 8. 16..
+//  Copyright (c) 2013년 jun. All rights reserved.
+//
+
+#import "EditViewController.h"
+
+@interface EditViewController ()
+
+@end
+
+@implementation EditViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
