277ee9629b31894849e2cd1237adcd8ca5737987
jun <hyun2012@gmail.com>
2013-08-16 14:24:29 +0900

    UIViewController의 코어 뷰 컨트롤러 클래스 생성 —> 공통 메소드와 속성 추가 —> 나머지 뷰 컨트롤러 클래스는 코어 뷰 컨트롤러 클래스를 상속하여 공통 메소드와 속성 자동 상속/구현할 수 있음.

---
+++ BirthdayReminder/CoreViewController.h
@@ -0,0 +1,13 @@
+//
+//  CoreViewController.h
+//  BirthdayReminder
+//
+//  Created by lovejun on 2013. 8. 16..
+//  Copyright (c) 2013년 jun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CoreViewController : UIViewController
+
+@end
---
+++ BirthdayReminder/CoreViewController.m
@@ -0,0 +1,38 @@
+//
+//  CoreViewController.m
+//  BirthdayReminder
+//
+//  Created by lovejun on 2013. 8. 16..
+//  Copyright (c) 2013년 jun. All rights reserved.
+//
+
+#import "CoreViewController.h"
+
+@interface CoreViewController ()
+
+@end
+
+@implementation CoreViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
