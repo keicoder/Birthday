a2d5cca410a420a4db7517933da06ada8be57fe3
jun <hyun2012@gmail.com>
2013-08-16 13:58:28 +0900

    편집 뷰에서 Cancel 버튼 클릭시 홈 뷰 컨트롤러로 되돌아오기 위한 unwind 메소드 생성 —> 홈 뷰 컨트롤러로 되돌아와야 하므로 홈 뷰 컨트롤러 내에 언와인드 액션을 선언하고 구현해야 함.

--- BirthdayReminder/HomeViewController.m
+++ BirthdayReminder/HomeViewController.m
@@ -14,6 +14,7 @@
 
 @implementation HomeViewController
 
+/*
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
@@ -22,6 +23,7 @@
     }
     return self;
 }
+*/
 
 - (void)viewDidLoad
 {
@@ -33,6 +35,14 @@
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - 생일 편집 뷰에서 Cancel 버튼을 눌렀을 때 홈 뷰 컨트롤러로 되돌아오기 위한 unwind 메소드.
+// 홈 뷰 컨트롤러로 되돌아와야 하므로 홈 뷰 컨트롤러 내에 언와인드 액션을 선언하고 구현해야 함.
+
+- (IBAction)unwindBackToHomeViewController:(UIStoryboardSegue *)segue
+{
+    NSLog(@"Cancel Button Clicked! : unwindBackToHomeViewController!");
 }
 
 @end
