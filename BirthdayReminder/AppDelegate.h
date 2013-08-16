7604f8dcee88c71c268d5bdda46c4a6449b6c034
jun <hyun2012@gmail.com>
2013-08-16 09:51:55 +0900

    Initial Commit

---
+++ BirthdayReminder/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  BirthdayReminder
+//
+//  Created by lovejun on 2013. 8. 16..
+//  Copyright (c) 2013년 jun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ BirthdayReminder/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  BirthdayReminder
+//
+//  Created by lovejun on 2013. 8. 16..
+//  Copyright (c) 2013년 jun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
