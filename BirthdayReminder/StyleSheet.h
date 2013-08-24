30d1279782e5abdd8068c11b2cef04edfcc3e058
jun <hyun2012@gmail.com>
2013-08-25 04:56:13 +0900

    앱의 모든 스타일을 한곳에서 집중적으로 관리해줄 수 있는 StyleSheet 클래스 생성 (그냥 단순한 NSObject의 하위 클래스)

---
+++ BirthdayReminder/StyleSheet.h
@@ -0,0 +1,13 @@
+//
+//  StyleSheet.h
+//  BirthdayReminder
+//
+//  Created by lovejun on 2013. 8. 25..
+//  Copyright (c) 2013년 jun. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface StyleSheet : NSObject
+
+@end
---
+++ BirthdayReminder/StyleSheet.m
@@ -0,0 +1,13 @@
+//
+//  StyleSheet.m
+//  BirthdayReminder
+//
+//  Created by lovejun on 2013. 8. 25..
+//  Copyright (c) 2013년 jun. All rights reserved.
+//
+
+#import "StyleSheet.h"
+
+@implementation StyleSheet
+
+@end
