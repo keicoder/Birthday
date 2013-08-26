50a7145d798c586d609a4128e9a6e67e7df78360
jun <hyun2012@gmail.com>
2013-08-26 13:22:17 +0900

    HomeViewController.h ActionButton 임포트 (친구의 정보를 가져오기 위한 버튼의 스타일 적용) 컨테이터 뷰, 라벨, 버튼의 속성 선언

--- BirthdayReminder/HomeViewController.h
+++ BirthdayReminder/HomeViewController.h
@@ -8,6 +8,7 @@
 
 #import <UIKit/UIKit.h>
 #import "CoreViewController.h"
+#import "ActionButton.h" // DeleteButton은 ActionButton 클래스의 하위 클래스 이므로 자동으로 버튼 스타일이 적용 됨
 
 
 // 데이블 뷰의 데이터소스 및 델리게이트로 선언
@@ -17,10 +18,30 @@
 // --> 여기서는 코어 데이터 저장소 내의 Birthday 엔티티 목록을 추적하고 이를 홈 뷰 컨트롤러에 보여줄 때 이 클래스 사용함
 // 홈 뷰 컨트롤러가 NSFetchedResultsControllerDelegate 프로토콜을 구현한다고 선언
 
+
 @interface HomeViewController : CoreViewController <UITableViewDataSource, UITableViewDelegate, NSFetchedResultsControllerDelegate>
 
 
 @property (weak, nonatomic) IBOutlet UITableView *tableView;
+
+@property (weak, nonatomic) IBOutlet UILabel *importLabel;
+@property (weak, nonatomic) IBOutlet ActionButton *addressBookButton;
+@property (weak, nonatomic) IBOutlet ActionButton *facebookButton;
+
+// 컨테이너 뷰(importView)에 대한 아웃렛은 생일 데이타베이스의 존재 여부에 따라 이 뷰를 보여주거나 숨기기 위해 필요함
+@property (weak, nonatomic) IBOutlet UIView *importView;
+
+
+#pragma mark - 최초 친구의 정보를 가져오기 위한 액션 메소드
+
+- (IBAction)importFromAddressBookTapped:(id)sender;
+- (IBAction)importFromFacebookTapped:(id)sender;
+
+
+#pragma mark - Segue 메소드
+
+-(IBAction)unwindBackToHomeViewController:(UIStoryboardSegue *)segue;
+
 
 
 @end
