52d7b951a47f9c33c83e4e99bdbcde9b35ca584b
jun <hyun2012@gmail.com>
2013-08-26 11:07:06 +0900

    버튼의 스킨 적용을 위해 UIButton의 하위 클래스로 ActionButton 클래스 생성 다시 ActionButton의 하위클래스로 DeleteButton 클래스 생성
    이 두 클래스는 스킨 용도로만 사용할 예정이므로 아무 코드도 입력하지 않아도 된다.

---
+++ BirthdayReminder/ActionButton.h
@@ -0,0 +1,19 @@
+//
+//  ActionButton.h
+//  BirthdayReminder
+//
+//  Created by lovejun on 2013. 8. 26..
+//  Copyright (c) 2013년 jun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+// 버튼의 스킨 적용을 위해 UIButton의 하위 클래스로 ActionButton 클래스 생성
+// 다시 ActionButton의 하위클래스로 DeleteButton 클래스 생성
+
+// 이 두 클래스는 스킨 용도로만 사용할 예정이므로 아무 코드도 입력하지 않아도 된다.
+
+
+@interface ActionButton : UIButton
+
+@end
---
+++ BirthdayReminder/DeleteButton.m
@@ -0,0 +1,31 @@
+//
+//  DeleteButton.m
+//  BirthdayReminder
+//
+//  Created by lovejun on 2013. 8. 26..
+//  Copyright (c) 2013년 jun. All rights reserved.
+//
+
+#import "DeleteButton.h"
+
+@implementation DeleteButton
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
