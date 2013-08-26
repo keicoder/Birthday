cad71ae9a45c13546363ba49ea6bf33c6e58b4cf
jun <hyun2012@gmail.com>
2013-08-26 14:23:36 +0900

    툴 바의 어드레스, 페이스북 버튼을 텝했을 때 보여 줄 모달 뷰의 컨트롤러인  ImportViewControlle 클래스 생성 - CoreViewController의 하위 클래스로 만듦.

---
+++ BirthdayReminder/ImportViewController.h
@@ -0,0 +1,13 @@
+//
+//  ImportViewController.h
+//  BirthdayReminder
+//
+//  Created by lovejun on 2013. 8. 26..
+//  Copyright (c) 2013년 jun. All rights reserved.
+//
+
+#import "CoreViewController.h"
+
+@interface ImportViewController : CoreViewController
+
+@end
