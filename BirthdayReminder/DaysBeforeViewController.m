3aeb13557be90a65e3f1867fe59e694de9d41652
jun <hyun2012@gmail.com>
2013-08-29 23:19:06 +0900

    DaysBeforeViewController.m DSettings.h 임포트 viewDidLoad, cellForRowAtIndexPath, numberOfRowsInSection, didSelectRowAtIndexPath 메소드 구현

--- BirthdayReminder/DaysBeforeViewController.m
+++ BirthdayReminder/DaysBeforeViewController.m
@@ -7,12 +7,58 @@
 //
 
 #import "DaysBeforeViewController.h"
+#import "DSettings.h"
+
 
 @interface DaysBeforeViewController ()
 
 @end
 
 @implementation DaysBeforeViewController
+
+
+-(void) viewDidLoad
+{
+    [super viewDidLoad];
+    UIImageView *backgroundView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"app-background.png"]];
+    [self.tableView setBackgroundView:backgroundView];
+}
+
+#pragma mark - UITableViewDataSource
+
+-(UITableViewCell *) tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"Cell"];
+    cell.textLabel.text = [[DSettings sharedInstance] titleForDaysBefore:indexPath.row];
+    //if this index path row is the stored days before setting then display a checkmark tick in the table cell
+    cell.accessoryType = ([DSettings sharedInstance].daysBefore == indexPath.row) ? UITableViewCellAccessoryCheckmark : UITableViewCellAccessoryNone;
+    return cell;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    //we could alternatively just return a total row count of 8 but this makes use of the last enumerated value to get the total count
+    return DaysBeforeTypeThreeWeeks + 1;
+}
+
+#pragma mark - UITableViewDelegate
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    
+    if (indexPath.row == [DSettings sharedInstance].daysBefore) {
+        //if it's the current ticked row then ignore
+        [tableView deselectRowAtIndexPath:indexPath animated:YES];
+        return;
+    }
+    
+    NSIndexPath *oldIndexPath = [NSIndexPath indexPathForRow:[DSettings sharedInstance].daysBefore inSection:0];
+    //Update the stored days before setting for the user
+    [DSettings sharedInstance].daysBefore = indexPath.row;
+    //The user has changed the selected days before row so we need to reload the table cells for the old and new rows
+    [tableView reloadRowsAtIndexPaths:@[oldIndexPath,indexPath] withRowAnimation:UITableViewRowAnimationNone];
+}
+
 
 
 @end
