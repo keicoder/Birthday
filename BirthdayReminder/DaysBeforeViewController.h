233bb6a6e4e27d7a3b54b3b2ef239f92e3ab10a0
jun <hyun2012@gmail.com>
2013-08-29 19:32:57 +0900

    DaysBeforeViewController 클래스 생성 스토리보드에서 클래스 설정 등 작업 스텁 코드 모두 제거

---
+++ BirthdayReminder/DaysBeforeViewController.h
@@ -0,0 +1,13 @@
+//
+//  DaysBeforeViewController.h
+//  BirthdayReminder
+//
+//  Created by lovejun on 2013. 8. 29..
+//  Copyright (c) 2013년 jun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DaysBeforeViewController : UITableViewController
+
+@end
---
+++ BirthdayReminder/DaysBeforeViewController.m
@@ -0,0 +1,18 @@
+//
+//  DaysBeforeViewController.m
+//  BirthdayReminder
+//
+//  Created by lovejun on 2013. 8. 29..
+//  Copyright (c) 2013년 jun. All rights reserved.
+//
+
+#import "DaysBeforeViewController.h"
+
+@interface DaysBeforeViewController ()
+
+@end
+
+@implementation DaysBeforeViewController
+
+
+@end
